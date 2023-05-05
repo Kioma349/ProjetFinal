927d18a8d9ea9f76f3e8299c3856de48055a3931
Linus <117490184+Kioma349@users.noreply.github.com>
2023-05-05 17:35:22 +0200

    a

--- tache3/Manipulation_Fichier/Main.java
+++ tache3/Manipulation_Fichier/Main.java
@@ -1,18 +1,24 @@
 package m1.finalee.tache3.Manipulation_Fichier;
 
-
 import java.io.File;
 
 import static m1.finalee.tache3.Manipulation_Fichier.ManipulationFichier.*;
 
 public class Main {
     public static void main(String[] args) {
+        // Crée un objet File représentant un répertoire
         File repertoire = new File("C:\\Users\\pc\\Pictures");
+        // Définit un préfixe pour filtrer les fichiers
         String prefix = "W";
 
+        // Affiche les fichiers du répertoire ayant le préfixe spécifié
         System.out.println("printFiles:");
         printFiles(repertoire, prefix);
+
+        // Calcule et affiche la taille totale des fichiers du répertoire
         System.out.println("\ntotalSizeOfFiles: " + totalSizeOfFiles(repertoire));
+
+        // Trouve et affiche le fichier le plus récent du répertoire
         System.out.println("\nmostRecentFile: " + mostRecentFile(repertoire));
     }
 }
